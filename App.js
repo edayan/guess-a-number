d911ee52e8577e7469680932a237a972f741d188
Saju Paul <saju.christel@blu-pa.com>
2020-08-14 01:24:35 +0530

    adding header

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header'
 export default function App() {
   return (
     <View>
-      
+      <Header title="Guess a number" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  
+
 });
