9abab108acc1f30c758b29323112f16e8e46dd81
Saju Paul <saju.christel@blu-pa.com>
2020-08-23 21:31:12 +0530

    implementing game and game over screen

--- App.js
+++ App.js
@@ -1,31 +1,43 @@
-import React , {useState} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
 
 import Header from './components/Header'
 import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
+
 export default function App() {
 
     const [userNumber, setUserNumber] = useState();
+    const [guessRounds, setGuessRounds] = useState(0);
 
     const startNameHandler = (selectedNumber) => {
         setUserNumber(selectedNumber);
+        setGuessRounds(0);
     }
 
-    let content = <StartGameScreen onStartGame={startNameHandler}/>
-    if (userNumber) {
-        content = <GameScreen userNumber={userNumber}/>
+    const gameOverHandler = numberOfRounds => {
+        setGuessRounds(numberOfRounds);
     }
-  return (
-    <View style={styles.screen}>
-      <Header title="Guess a number" />
-        {content}
-    </View>
-  );
+
+
+    let content = <StartGameScreen onStartGame={startNameHandler}/>
+    if (userNumber && guessRounds <= 0) {
+        content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+    } else if (guessRounds > 0) {
+        content = <GameOverScreen/>
+    }
+
+    return (
+        <View style={styles.screen}>
+            <Header title="Guess a number"/>
+            {content}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-    screen : {
-        flex : 1
+    screen: {
+        flex: 1
     }
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,12 +1,12 @@
-import React, {useState} from 'react'
-import {StyleSheet, View, Text, Button} from 'react-native'
+import React, {useEffect, useRef, useState} from 'react'
+import {Alert, Button, StyleSheet, Text, View} from 'react-native'
 import NumberContainer from "../components/NumberContainer";
 import Card from "../components/Card";
 
 
 const generateRandomBetween = (min, max, exclude) => {
     min = Math.ceil(min);
-    max = Math.floor(min);
+    max = Math.floor(max);
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
     if (rndNum === exclude) {
         return generateRandomBetween(min, max, exclude);
@@ -14,29 +14,62 @@ const generateRandomBetween = (min, max, exclude) => {
         return rndNum;
     }
 }
+
 const GameScreen = props => {
     const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 99, props.userChoice))
+    const [rounds, setRounds] = useState(0);
 
+    const currentLow = useRef(1);
+    const currentHigh = useRef(100);
+
+    const nextGuessHandler = direction => {
+        if ((direction === 'lower' && props.userChoice > currentGuess)
+            || (direction === 'greater' && props.userChoice < currentGuess)) {
+            Alert.alert('Don\'t lie', 'You know you choose wrong direction',
+                [{text: 'Sorry', style: 'cancel'}]);
+            return
+        }
+
+        if (direction === 'lower') {
+            currentHigh.current = currentGuess;
+        } else if (direction === 'greater') {
+            currentLow.current = currentGuess;
+        }
+        const nextGuess = generateRandomBetween(currentLow.current, currentHigh.current, currentGuess);
+        setCurrentGuess(nextGuess);
+        setRounds(currentRounds => currentRounds + 1);
+    }
+
+    const {userChoice, onGameOver} = props;
+    useEffect(() => {
+        if (currentGuess === userChoice) {
+            onGameOver(rounds);
+        }
+    }, [currentGuess, userChoice, onGameOver]);
 
     return (
         <View style={styles.screen}>
             <Text>Opponents guess</Text>
             <NumberContainer>{currentGuess}</NumberContainer>
             <Card style={styles.buttonContainer}>
-                <Button title="LOWER" onPress={() => {}}/>
-                <Button title="GREATER" onPress={() => {}}/>
+                <Button title="LOWER" onPress={() => {
+                    nextGuessHandler('lower')
+                }}/>
+                <Button title="GREATER" onPress={() => {
+                    nextGuessHandler('greater')
+                }}/>
             </Card>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    screen : {
-        flex :1,
+    screen: {
+        flex: 1,
         padding: 10,
         alignItems: 'center'
     },
-    buttonContainer : {
+    buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         marginTop: 20,
