cb1b7d264723d65a57ba6965f0cef4d8b5e987b2
Saju Paul <saju.christel@blu-pa.com>
2020-08-24 00:49:55 +0530

    installing new fonts and using those

--- App.js
+++ App.js
@@ -1,15 +1,30 @@
 import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
+import * as Font from 'expo-font';
+import {AppLoading} from 'expo'
 
 import Header from './components/Header'
 import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
 
-export default function App() {
+const fetchFonts = () => {
+    return Font.loadAsync({
+        'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+        'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    });
+}
 
+export default function App() {
     const [userNumber, setUserNumber] = useState();
     const [guessRounds, setGuessRounds] = useState(0);
+    const [dataLoaded, setDataLoaded] = useState(false);
+
+    if (!dataLoaded) {
+        return <AppLoading startAsync={fetchFonts}
+                           onFinish={() => setDataLoaded(true)}
+                           onError={err => console.log(err)}/>
+    }
 
     const startNameHandler = (selectedNumber) => {
         setUserNumber(selectedNumber);
@@ -27,10 +42,13 @@ export default function App() {
 
     let content = <StartGameScreen onStartGame={startNameHandler}/>
     if (userNumber && guessRounds <= 0) {
-        content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+        content = <GameScreen
+            userChoice={userNumber}
+            onGameOver={gameOverHandler}/>
     } else if (guessRounds > 0) {
-        content = <GameOverScreen numberOfRounds={guessRounds} userNumber={userNumber}
-                                  onNewGame={newGameHandler}/>
+        content = <GameOverScreen
+            numberOfRounds={guessRounds}  userNumber={userNumber}
+            onNewGame={newGameHandler}/>
     }
 
     return (
--- components/Header.js
+++ components/Header.js
@@ -22,7 +22,8 @@ const styles = StyleSheet.create({
     },
     headerTitle: {
         color: `white`,
-        fontSize: 18
+        fontSize: 18,
+        fontFamily: 'open-sans-bold'
     }
 });
 
